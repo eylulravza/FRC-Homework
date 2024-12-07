b5fa9ca2d20ef1342d5d3b3513a4be15877c814a
eylulravza <sevdagungul@gmail.com>
2024-12-07 21:30:36 +0300

    Delete public class Hesaplama {.java

--- public class Hesaplama {.java
+++
@@ -1,9 +0,0 @@
-public class Hesaplama {
-    public static void main (String[] args) {
-        int sayi1= 15;
-        int sayi2= 3;
-        
-        int sonuc1= sayi1 + sayi2;
-        System.out.println(sonuc1);
-    }
-}
